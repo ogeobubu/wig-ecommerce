13b18ae709d90d3f52872bd9b2c7d446e3e4f916
Oge Obubu <ogeobubu@gmail.com>
2021-10-25 01:53:47 +0100

    recent uodate

--- controllers/product.js
+++ controllers/product.js
@@ -4,7 +4,7 @@ exports.createProducts = async (req, res) => {
   const { title, description, image, categories, hairLength, color, price } =
     req.body;
   try {
-    const newProduct = await new Product({
+    const newProduct = {
       title,
       description,
       image,
@@ -12,13 +12,49 @@ exports.createProducts = async (req, res) => {
       hairLength,
       color,
       price,
-    }).save();
+    };
+    const savedProduct = await new Product(newProduct).save();
 
     res.status(200).json({
-      message: "New product has been created.",
+      message: savedProduct,
     });
   } catch (error) {
-    return res.status(400).json({
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = req.query.latest
+      ? await Product.find().sort({ _id: -1 }).limit(5)
+      : await Product.find();
+
+    res.status(200).json({
+      message: products,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(400).json({
+        message: "This product does not exist!",
+      });
+    } else {
+      res.status(200).json({
+        message: product,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
       message: error.message,
     });
   }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { createProducts } = require("../controllers/product");
+const {
+  createProducts,
+  getProducts,
+  getProduct,
+} = require("../controllers/product");
 const admin = require("../middlewares/admin");
 const auth = require("../middlewares/auth");
 
-router.post("/", createProducts);
+router.post("/", auth, admin, createProducts);
+router.get("/all", auth, getProducts);
+router.get("/all", auth, getProducts);
+router.get("/:id", auth, getProduct);
 
 module.exports = router;
