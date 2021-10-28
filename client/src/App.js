d7a60fc6d7647121cd17afde72644b09d0e6679a
Oge Obubu <ogeobubu@gmail.com>
2021-10-28 04:08:41 +0100

    added react-router-dom to naviagate between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-// import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
-// import Register from "./pages/Register";
-// import Login from "./pages/Login";
-// import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 function App() {
-  return <Home />;
+  const user = false;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products/:category" component={ProductList} />
+        <Route exact path="/products/:category" component={ProductList} />
+        <Route exact path="/product/:id" component={Product} />
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/login" component={user ? Home : Login} />
+        <Route exact path="/create" component={user ? Home : Register} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
